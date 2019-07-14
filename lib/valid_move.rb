358c201410c793b7e8e1152072c37966974aafba
Aude Faucheux <aude53@gmail.com>
2019-07-14 09:18:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index].to_i >= 0 && board[index].to_i <= 8
-    true
+  if board[index].to_i <= 0 && board[index].to_i >= 8
+    def position_taken?(board, index)
+      if board[index] == " " || board[index] == "" || board[index] == nil
+        false
+      end
+    end
   end
 end
 
